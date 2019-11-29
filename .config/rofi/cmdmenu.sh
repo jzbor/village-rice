320d4e0f1d5612bf1c3fec1a7aba9fef6a2f8deb
jzbor <jzbor@posteo.net>
2019-11-29 19:46:58 +0100

    Initial commit

---
+++ .config/rofi/cmdmenu.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+bg_color=#d02f343f
+trans_color=#00000000
+htext_color=#fed7ae
+text_color=#bfbfa1
+
+rofi -show run -theme cmdmenu\
+    -terminal terminator -ssh-command '{terminal} -e "{ssh-client} {host}"' \
+    $@
---
+++ .config/rofi/drun.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+bg_color=#d02f343f
+trans_color=#00000000
+htext_color=#fed7ae
+text_color=#bfbfa1
+
+rofi -show drun -theme village\
+    -terminal terminator -ssh-command '{terminal} -e "{ssh-client} {host}"' \
+    $@
